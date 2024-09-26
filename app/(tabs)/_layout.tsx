a23866c0a0245232a31cf9df1636c8f02d6a75ac
Pradip Debnath <itzpradip@gmail.com>
2024-09-26 20:47:04 +0530

    updated tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,41 @@
+import React from 'react';
 import { Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
-    <Tabs />
+    <Tabs screenOptions={{headerShown: false}}>
+      <Tabs.Screen name='index' options={{
+        title: 'Home',
+        tabBarIcon: ({color}) => (
+          <Ionicons name='home-outline' size={22} color={color} />
+        )
+      }} />
+      <Tabs.Screen name='explore' options={{
+        title: 'Explore',
+        tabBarIcon: ({color}) => (
+          <Ionicons name='search-outline' size={22} color={color} />
+        )
+      }} />
+      <Tabs.Screen name='notifications' options={{
+        title: 'Notification',
+        tabBarIcon: ({color}) => (
+          <Ionicons name='notifications-outline' size={22} color={color} />
+        )
+      }} />
+      <Tabs.Screen name='cart' options={{
+        title: 'Cart',
+        tabBarBadge: 3,
+        tabBarIcon: ({color}) => (
+          <Ionicons name='cart-outline' size={22} color={color} />
+        )
+      }} />
+      <Tabs.Screen name='profile' options={{
+        title: 'Profile',
+        tabBarIcon: ({color}) => (
+          <Ionicons name='person-outline' size={22} color={color} />
+        )
+      }} />
+    </Tabs>
   );
 }
