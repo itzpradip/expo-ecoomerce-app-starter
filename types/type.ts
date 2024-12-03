6356b80f46ca3673a43ec6112cd49e7fbbdfa9dc
Pradip Debnath <itzpradip@gmail.com>
2024-12-03 22:28:45 +0530

    added cart & notification data to db.json

--- types/type.ts
+++ types/type.ts
@@ -18,3 +18,18 @@ export interface CategoryType {
   name: string;
   image: string;
 }
+
+export interface CartItemType {
+  id: number;
+  title: string;
+  price: number;
+  quantity: number;
+  image: string;
+}
+
+export interface NotificationType {
+  id: number;
+  title: string;
+  message: string;
+  timestamp: string;
+}
